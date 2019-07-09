c863c908e000d7fb78605e3caaf1569efe5a508e
unknown <georg.digmelashvili@gmail.com>
2019-07-09 13:06:52 +0400

    Created

---
+++ MusicPlayer/MusicPlayer/MediaPlayer.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MusicPlayer
+{
+    partial class MediaPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MediaPlayer));
+            this.WMPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.MusicList = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.WMPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WMPlayer
+            // 
+            this.WMPlayer.Enabled = true;
+            this.WMPlayer.Location = new System.Drawing.Point(173, 1);
+            this.WMPlayer.Name = "WMPlayer";
+            this.WMPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMPlayer.OcxState")));
+            this.WMPlayer.Size = new System.Drawing.Size(615, 541);
+            this.WMPlayer.TabIndex = 0;
+            // 
+            // MusicList
+            // 
+            this.MusicList.AllowDrop = true;
+            this.MusicList.FormattingEnabled = true;
+            this.MusicList.Location = new System.Drawing.Point(794, 12);
+            this.MusicList.Name = "MusicList";
+            this.MusicList.Size = new System.Drawing.Size(282, 394);
+            this.MusicList.TabIndex = 2;
+            this.MusicList.DragDrop += new System.Windows.Forms.DragEventHandler(this.MusicList_DragDrop);
+            this.MusicList.DragEnter += new System.Windows.Forms.DragEventHandler(this.MusicList_DragEnter);
+            this.MusicList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListBox1_MouseDoubleClick);
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.AddButton.FlatAppearance.BorderSize = 0;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.Location = new System.Drawing.Point(794, 504);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(108, 38);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.Color.DarkGray;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 1);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(175, 541);
+            this.MenuPanel.TabIndex = 4;
+            // 
+            // MediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1088, 543);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.MusicList);
+            this.Controls.Add(this.WMPlayer);
+            this.Name = "MediaPlayer";
+            this.Text = "Media Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MediaPlayer_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.WMPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer WMPlayer;
+        private System.Windows.Forms.ListBox MusicList;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel MenuPanel;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/MediaPlayer.cs
@@ -0,0 +1,82 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class MediaPlayer : Form
+    {
+        int start = 0;
+
+        public MediaPlayer()
+        {
+            InitializeComponent();
+            this.MusicList.DragDrop += new
+           System.Windows.Forms.DragEventHandler(this.MusicList_DragDrop);
+            this.MusicList.DragEnter += new
+                       System.Windows.Forms.DragEventHandler(this.MusicList_DragEnter);
+        }
+
+        public List<string> Files = new List<string>();
+        public List<string> Path = new List<string>();
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            start = FileAdd(start);
+        }
+        
+        private int FileAdd(int start)
+        {
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Files.AddRange(openFileDialog.SafeFileNames);
+                Path.AddRange(openFileDialog.FileNames);
+                
+                    for (int i = start; i < Files.Count; i++)
+                    {
+                        MusicList.Items.Add(Files[i]);
+                        start++;
+                    }
+
+            }
+            return start;
+        }
+
+
+        private void ListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            WMPlayer.URL = Path[MusicList.SelectedIndex];
+        }
+
+        private void MediaPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WMPlayer.Dispose();
+        }
+
+        private void MusicList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void MusicList_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                MusicList.Items.Add(s[i]);
+        }
+    }
+}
